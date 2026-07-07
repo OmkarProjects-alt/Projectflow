8d0f1bf4c40ddebc4b53947fa175cca844d79744
omkarGudappe <omkar.gudappe205@gmail.com>
2026-07-08 01:52:33 +0530

    cookie updated

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -10,8 +10,11 @@ const generateToken = (res, userId) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite:
+            process.env.NODE_ENV === "production"
+                ? "none"
+                : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 }
